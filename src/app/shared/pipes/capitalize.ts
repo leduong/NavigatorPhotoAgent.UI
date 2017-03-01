9683e6e624941ea2a11fc175a9ccc929bf572ca3
Le Duong <me@leduong.com>
2017-03-01 13:33:57 +0700

    Update 2017-03-01T06:33:51.245Z

--- src/app/shared/pipes/capitalize.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'capitalize' })
-export class CapitalizePipe implements PipeTransform {
-
-  transform(input: string): string {
-    return (!input || input === '') ? input : (input.charAt(0).toUpperCase() + input.slice(1));
-  }
-}
