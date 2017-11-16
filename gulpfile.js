e62eb933b8300efc73ca07d0d5ae20c8d3fc2d38
magnastrazh <magnastrazh@gmail.com>
2017-11-16 19:08:43 +0300

    issue 25   auth config
    issue 25
    
    added auth config
    set AUTHORITY=https://auth.informationcart.com
    set CLIENT_ID=navigatorphotoagentui-dev
    set REDIRECT_URI=http://localhost:8000/callback.html
    set RESPONSE_TYPE=id_token token
    set SCOPE=openid profile roles imagegalleryapi country subscriptionlevel
    set POST_LOGOUT_REDIRECT_URI=http://localhost:8000/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,24 @@
 "use strict";
 
 const fs = require('fs'),
-  del = require('del'),
-  path = require('path'),
-  gulp = require('gulp'),
-  iF = require('gulp-if'),
-  less = require('gulp-less'),
-  // sass = require('gulp-sass'),
-  concat = require('gulp-concat'),
-  cssmin = require('gulp-cssmin'),
-  tslint = require('gulp-tslint'),
-  tsc = require('gulp-typescript'),
-  replace = require('gulp-replace'),
-  jsMinify = require('gulp-uglify'),
-  ghPages = require('gulp-gh-pages'),
-  tsconfig = require('gulp-ts-config'),
-  coveralls = require('gulp-coveralls'),
-  sourcemaps = require('gulp-sourcemaps'),
-  cssPrefixer = require('gulp-autoprefixer'),
-  merge = require('merge-stream');
+    del = require('del'),
+    path = require('path'),
+    gulp = require('gulp'),
+    iF = require('gulp-if'),
+    less = require('gulp-less'),
+    // sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    tslint = require('gulp-tslint'),
+    tsc = require('gulp-typescript'),
+    replace = require('gulp-replace'),
+    jsMinify = require('gulp-uglify'),
+    ghPages = require('gulp-gh-pages'),
+    tsconfig = require('gulp-ts-config'),
+    coveralls = require('gulp-coveralls'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssPrefixer = require('gulp-autoprefixer'),
+    merge = require('merge-stream');
 
 const SystemBuilder = require('systemjs-builder');
 const tsProject = tsc.createProject('tsconfig.json');
@@ -33,54 +33,62 @@ var ATTRACTION = process.env.ATTRACTION || '/api/attraction';
 var MAPSAPI = process.env.MAPSAPI || '/api/maps';
 var REPORTSAPI = process.env.REPORTSAPI || '/api/reports';
 
+//OAuth
+var AUTHORITY = process.env.AUTHORITY || '';
+var CLIENT_ID = process.env.CLIENT_ID || '';
+var REDIRECT_URI = process.env.REDIRECT_URI || '';
+var RESPONSE_TYPE = process.env.RESPONSE_TYPE || '';
+var SCOPE = process.env.SCOPE || '';
+var POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || '';
+
 var build = false;
-process.argv.forEach(function(val, index, array) {
-  if ('build' === val) {
-    build = true;
-  }
+process.argv.forEach(function (val, index, array) {
+    if ('build' === val) {
+        build = true;
+    }
 });
 /**
  * Remove build directory.
  */
 gulp.task('clean', (cb) => {
-  return del([buildDir, '.tmp'], cb);
+    return del([buildDir, '.tmp'], cb);
 });
 
-gulp.task('ghpage', function() {
-  return gulp.src('./' + buildDir + '/**/*')
-    .pipe(ghPages());
+gulp.task('ghpage', function () {
+    return gulp.src('./' + buildDir + '/**/*')
+        .pipe(ghPages());
 });
 
 /**
  * Compile all SASS files.
  */
-gulp.task('sass', function() {
-  return gulp.src('src/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cssmin())
-    .pipe(gulp.dest(path.join(buildDir, "css")));
+gulp.task('sass', function () {
+    return gulp.src('src/sass/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.join(buildDir, "css")));
 });
 
 /**
  * Compile all Less files.
  */
-gulp.task("less", function() {
-  return gulp
-    .src(["src/less/*.less", "src/less/themes/*.less"])
-    .pipe(less())
-    .pipe(cssmin())
-    .pipe(gulp.dest(path.join(buildDir, "css")));
+gulp.task("less", function () {
+    return gulp
+        .src(["src/less/*.less", "src/less/themes/*.less"])
+        .pipe(less())
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.join(buildDir, "css")));
 });
 
 /**
  * Lint all custom TypeScript files.
  */
 gulp.task('tslint', () => {
-  return gulp.src("src/**/*.ts")
-    .pipe(tslint({
-      formatter: "verbose"
-    }))
-    .pipe(tslint.report());
+    return gulp.src("src/**/*.ts")
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report());
 });
 
 
@@ -89,33 +97,33 @@ gulp.task('tslint', () => {
  */
 
 gulp.task('shims', () => {
-  return gulp.src([
-      'node_modules/core-js/client/shim.min.js',
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/reflect-metadata/Reflect.js',
-      'node_modules/systemjs/dist/system.src.js'
+    return gulp.src([
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js'
     ])
-    .pipe(concat('shims.js'))
-    .pipe(iF(build, jsMinify()))
-    .pipe(gulp.dest(path.join(buildDir, 'js')));
+        .pipe(concat('shims.js'))
+        .pipe(iF(build, jsMinify()))
+        .pipe(gulp.dest(path.join(buildDir, 'js')));
 });
 
 gulp.task('tsc', ['tslint'], () => {
-  var tsDest = (NG_ENVIRONMENT === 'Dev') ? (buildDir + '/app') : '.tmp/src/app';
-  var tsProject = tsc.createProject('tsconfig.json'),
-    tsResult = tsProject.src().pipe(tsProject());
-  // .pipe(tsc(tsProject));
+    var tsDest = (NG_ENVIRONMENT === 'Dev') ? (buildDir + '/app') : '.tmp/src/app';
+    var tsProject = tsc.createProject('tsconfig.json'),
+        tsResult = tsProject.src().pipe(tsProject());
+    // .pipe(tsc(tsProject));
 
-  return tsResult.js.pipe(gulp.dest(tsDest));
+    return tsResult.js.pipe(gulp.dest(tsDest));
 });
 
 gulp.task('compile', ['tsc'], () => {
-  if (NG_ENVIRONMENT !== 'Dev') {
-    var builder = new SystemBuilder();
-    return builder.loadConfig('systemjs.config.js')
-      .then(() => builder.buildStatic('app', path.join(buildDir, 'js', 'bundle.js')));
-  }
-  return;
+    if (NG_ENVIRONMENT !== 'Dev') {
+        var builder = new SystemBuilder();
+        return builder.loadConfig('systemjs.config.js')
+            .then(() => builder.buildStatic('app', path.join(buildDir, 'js', 'bundle.js')));
+    }
+    return;
 });
 
 
@@ -123,8 +131,8 @@ gulp.task('compile', ['tsc'], () => {
  * Copy all resources that are not TypeScript files into build directory.
  */
 gulp.task("resources", () => {
-  return gulp.src(["!src/index.html", "!src/less", "!src/less/**/*", "!**/*.ts", "src/**/*"])
-    .pipe(gulp.dest(buildDir));
+    return gulp.src(["!src/index.html", "!src/less", "!src/less/**/*", "!**/*.ts", "src/**/*"])
+        .pipe(gulp.dest(buildDir));
 });
 
 
@@ -132,127 +140,157 @@ gulp.task("resources", () => {
  * Copy all required fonts into build directory.
  */
 gulp.task('fonts', () => {
-  return gulp.src([
-      'bootstrap/fonts/**',
-      'font-awesome/fonts/**',
-      'simple-line-icons/fonts/**'
+    return gulp.src([
+        'bootstrap/fonts/**',
+        'font-awesome/fonts/**',
+        'simple-line-icons/fonts/**'
     ], { cwd: 'node_modules' })
-    .pipe(gulp.dest(path.join(buildDir, 'fonts')));
+        .pipe(gulp.dest(path.join(buildDir, 'fonts')));
 });
 
 /**
  * Watch for changes in TypeScript, HTML and CSS files.
  */
 gulp.task('watch', () => {
-  gulp.watch(['src/**/**.ts'], ['compile']).on('change', function(e) {
-    console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
-  });
-  gulp.watch(['src/**/**.html', 'src/**/*.css', 'src/img/*.*'], ['resources']).on('change', function(e) {
-    console.log('Resource file ' + e.path + ' has been changed. Updating.');
-  });
-  gulp.watch(['src/**/**.less'], ['less']).on('change', function(e) {
-    console.log('LESS file ' + e.path + ' has been changed. Updating.');
-  });
-  // gulp.watch(['src/**/**.scss'], ['less']).on('change', function(e) {
-  //   console.log('SASS file ' + e.path + ' has been changed. Updating.');
-  // });
+    gulp.watch(['src/**/**.ts'], ['compile']).on('change', function (e) {
+        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+    });
+    gulp.watch(['src/**/**.html', 'src/**/*.css', 'src/img/*.*'], ['resources']).on('change', function (e) {
+        console.log('Resource file ' + e.path + ' has been changed. Updating.');
+    });
+    gulp.watch(['src/**/**.less'], ['less']).on('change', function (e) {
+        console.log('LESS file ' + e.path + ' has been changed. Updating.');
+    });
+    // gulp.watch(['src/**/**.scss'], ['less']).on('change', function(e) {
+    //   console.log('SASS file ' + e.path + ' has been changed. Updating.');
+    // });
 });
 
-gulp.task('appsettings', function(cb) {
-  var build = 'build: ' + BUILD;
-  var ng2ENV = '\nng2ENV: ' + NG_ENVIRONMENT;
-  var apiendpoint = '\nApiEndpoint: ' + APIENDPOINT;
-  var landmark = '\nApiLandmarks: ' + LANDMARK;
-  var maps = '\nApiMaps: ' + MAPSAPI;
-  var reports = '\nApiReports: ' + REPORTSAPI;
-  var attraction = '\nApiAttraction: ' + ATTRACTION;
-  return fs.writeFile('appsettings.yml', build + ng2ENV + apiendpoint + landmark + maps + reports + attraction, cb);
+gulp.task('appsettings', function (cb) {
+    var build = 'build: ' + BUILD;
+    var ng2ENV = '\nng2ENV: ' + NG_ENVIRONMENT;
+    var apiendpoint = '\nApiEndpoint: ' + APIENDPOINT;
+    var landmark = '\nApiLandmarks: ' + LANDMARK;
+    var maps = '\nApiMaps: ' + MAPSAPI;
+    var reports = '\nApiReports: ' + REPORTSAPI;
+    var attraction = '\nApiAttraction: ' + ATTRACTION;
+    return fs.writeFile('appsettings.yml', build + ng2ENV + apiendpoint + landmark + maps + reports + attraction, cb);
 });
 
-gulp.task('api', function() {
-  return gulp.src('appsettings.yml')
-    .pipe(tsconfig('AppSettings', JSON.parse('{"parser": "yml"}')))
-    .pipe(gulp.dest('./src/app/routes'))
+gulp.task('api', function () {
+    return gulp.src('appsettings.yml')
+        .pipe(tsconfig('AppSettings', JSON.parse('{"parser": "yml"}')))
+        .pipe(gulp.dest('./src/app/routes'))
 });
 
-
-gulp.task('bundle', function() {
-  var bundleTpl;
-  if (NG_ENVIRONMENT === 'Dev') {
-    bundleTpl = '<script src="systemjs.config.js"></script>' +
-      '<script>System.import(\'app\').catch(function(err) {console.error(err);});</script>';
-  } else {
-    bundleTpl = '<script type="text/javascript" src="js/bundle.js"></script>';
-  }
-
-  return gulp.src('src/index.html')
-    .pipe(replace('<--bundleTpl-->', bundleTpl))
-    .pipe(replace('#{APIENDPOINT}', APIENDPOINT))
-    .pipe(replace('#{MAPSAPI}', MAPSAPI))
-    .pipe(replace('#{ng2ENV}', NG_ENVIRONMENT))
-    .pipe(gulp.dest(buildDir));
+gulp.task('oauthsettings', function (cb) {
+    var authority = 'authority: ' + AUTHORITY;
+    var client_id = '\nclient_id: ' + CLIENT_ID;
+    var redirect_uri = '\nredirect_uri: ' + REDIRECT_URI;
+    var response_type = '\nresponse_type: ' + RESPONSE_TYPE;
+    var scope = '\nscope: ' + SCOPE;
+    var post_logout_redirect_uri = '\npost_logout_redirect_uri: ' + POST_LOGOUT_REDIRECT_URI;
+    return fs.writeFile('oauthsettings.yml', authority
+        + client_id
+        + redirect_uri
+        + response_type
+        + scope
+        + post_logout_redirect_uri);
 });
 
-gulp.task('systemjs', function() {
-  if (NG_ENVIRONMENT === 'Dev') {
-    return gulp.src('systemjs.config.js')
-      .pipe(replace('.tmp/src/app', 'app'))
-      .pipe(gulp.dest(buildDir));
-  }
-  return;
+gulp.task('oauthconf', function () {
+    return gulp.src('oauthsettings.yml')
+        .pipe(tsconfig('OAuthSettings', JSON.parse('{"parser": "yml"}')))
+        .pipe(gulp.dest('./src/app'))
+});
+
+gulp.task('bundle', function () {
+    var bundleTpl;
+    if (NG_ENVIRONMENT === 'Dev') {
+        bundleTpl = '<script src="systemjs.config.js"></script>' +
+            '<script>System.import(\'app\').catch(function(err) {console.error(err);});</script>';
+    } else {
+        bundleTpl = '<script type="text/javascript" src="js/bundle.js"></script>';
+    }
+
+    return gulp.src('src/index.html')
+        .pipe(replace('<--bundleTpl-->', bundleTpl))
+        .pipe(replace('#{APIENDPOINT}', APIENDPOINT))
+        .pipe(replace('#{MAPSAPI}', MAPSAPI))
+        .pipe(replace('#{ng2ENV}', NG_ENVIRONMENT))
+        //OAuth
+        .pipe(replace('#{authority}', AUTHORITY))
+        .pipe(replace('#{client_id}', CLIENT_ID))
+        .pipe(replace('#{redirect_uri}', REDIRECT_URI))
+        .pipe(replace('#{response_type}', RESPONSE_TYPE))
+        .pipe(replace('#{scope}', SCOPE))
+        .pipe(replace('#{post_logout_redirect_uri}', POST_LOGOUT_REDIRECT_URI))
+
+        .pipe(gulp.dest(buildDir));
+});
+
+gulp.task('systemjs', function () {
+    if (NG_ENVIRONMENT === 'Dev') {
+        return gulp.src('systemjs.config.js')
+            .pipe(replace('.tmp/src/app', 'app'))
+            .pipe(gulp.dest(buildDir));
+    }
+    return;
 });
 
 /**
  * Copy all required libraries into build directory.
  */
 gulp.task("node_modules", () => {
-  if (NG_ENVIRONMENT === 'Dev') {
-    return gulp.src([
-        'rxjs/**',
-        'core-js/**',
-        'zone.js/dist/**',
-        'systemjs/dist/**',
-        //'ng2-select/**',
-        // 'ng2-dnd/**',
-        'ngx-infinite-scroll/**',
-        // 'angular2-toaster/**',
-        // 'angular2-google-maps/**',
-        // 'ng2-translate/**',
-        //'ng2-table/**',
-        'ngx-bootstrap/**',
-        'screenfull/dist/screenfull.js',
-        'jquery/dist/jquery.js',
-        'jquery.browser/dist/jquery.browser.js',
-        'moment/moment.js',
-        '@angular/**'
-      ], { cwd: "node_modules/**" }) /* Glob required here. */
-      .pipe(gulp.dest(path.join(buildDir, "node_modules")));
-  }
-  return;
+    if (NG_ENVIRONMENT === 'Dev') {
+        return gulp.src([
+            'rxjs/**',
+            'core-js/**',
+            'zone.js/dist/**',
+            'systemjs/dist/**',
+            //'ng2-select/**',
+            // 'ng2-dnd/**',
+            'ngx-infinite-scroll/**',
+            // 'angular2-toaster/**',
+            // 'angular2-google-maps/**',
+            // 'ng2-translate/**',
+            //'ng2-table/**',
+            'ngx-bootstrap/**',
+            'screenfull/dist/screenfull.js',
+            'jquery/dist/jquery.js',
+            'jquery.browser/dist/jquery.browser.js',
+            'moment/moment.js',
+            '@angular/**'
+        ], { cwd: "node_modules/**" }) /* Glob required here. */
+            .pipe(gulp.dest(path.join(buildDir, "node_modules")));
+    }
+    return;
 });
 
 /**
  * Build the project.
  */
 gulp.task("build", [
-  'appsettings',
-  'api',
-  'compile',
-  'shims',
-  'less',
-  // 'sass',
-  'fonts',
-  'resources',
-  'node_modules',
-  'systemjs',
-  'bundle'
+    'appsettings',
+    'api',
+    'oauthsettings',
+    'oauthconf',
+    'compile',
+    'shims',
+    'less',
+    // 'sass',
+    'fonts',
+    'resources',
+    'node_modules',
+    'systemjs',
+    'bundle'
 ], () => {
-  console.log('Building the project ...');
-  if (NG_ENVIRONMENT !== 'Dev') {
-    return gulp.src(path.join(buildDir, 'js', 'bundle.js'))
-      .pipe(iF(build, jsMinify()))
-      .pipe(gulp.dest(path.join(buildDir, 'js')));
-  }
-  return;
+    console.log('Building the project ...');
+    if (NG_ENVIRONMENT !== 'Dev') {
+        return gulp.src(path.join(buildDir, 'js', 'bundle.js'))
+            .pipe(iF(build, jsMinify()))
+            .pipe(gulp.dest(path.join(buildDir, 'js')));
+    }
+    return;
 
 });
--- src/app/oauthsettings.ts
+++ src/app/oauthsettings.ts
@@ -1,20 +1,21 @@
-﻿export class OAuthSettings {
-    public static get authority(): string {
-        return "https://auth.informationcart.com";
-    }
-    public static get client_id(): string {
-        return "navigatorphotoagentui-dev";
-    }
-    public static get redirect_uri(): string {
-        return "http://localhost:8000/callback.html";
-    }
-    public static get response_type(): string {
-        return "id_token token";
-    }
-    public static get scope(): string {
-        return "openid profile roles imagegalleryapi country subscriptionlevel";
-    }
-    public static get post_logout_redirect_uri(): string {
-        return "http://localhost:8000/index.html";
-    }
+export class OAuthSettings {
+
+  public static get authority(): string {
+    return "https://auth.informationcart.com";
+  }
+  public static get client_id(): string {
+    return "navigatorphotoagentui-dev";
+  }
+  public static get redirect_uri(): string {
+    return "http://localhost:8000/callback.html";
+  }
+  public static get response_type(): string {
+    return "id_token token";
+  }
+  public static get scope(): string {
+    return "openid profile roles imagegalleryapi country subscriptionlevel";
+  }
+  public static get post_logout_redirect_uri(): string {
+    return "http://localhost:8000/index.html";
+  }
 }
