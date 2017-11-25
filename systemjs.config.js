43709ceaa7d3047c2489d808c506a43b1176b8a4
magnastrazh <magnastrazh@gmail.com>
2017-11-25 06:22:06 +0300

    and again

--- systemjs.config.js
+++ systemjs.config.js
@@ -37,7 +37,8 @@ System.config({
         // 'ng2-bootstrap': 'libs:ng2-bootstrap'
         // 'ng2-table': 'libs:ng2-table',
         'ng2-ace-editor': 'libs:ng2-ace-editor',
-        'brace': 'libs:brace'
+        'brace': 'libs:brace',
+        'ngx-bootstrap/bs-moment': 'libs:moment'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -53,6 +54,7 @@ System.config({
         'ng2-ace-editor': { main: 'index.js', defaultExtension: 'js' },
         'brace': { main: 'index.js', defaultExtension: 'js' },
         "rxjs/operators": { main: "index.js", defaultExtension: "js" },
-        moment: { main: 'moment.js',defaultExtension: 'js' }
+        moment: { main: 'moment.js', defaultExtension: 'js' },
+        'ngx-bootstrap/bs-moment': { main: 'moment.js', defaultExtension: 'js' },
     }
 });
