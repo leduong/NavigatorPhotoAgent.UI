7c3b5fec88f8752fe644c405f4008c2e37211f0e
magnastrazh <magnastrazh@gmail.com>
2017-12-18 19:00:39 +0300

    recaptcha = issue3

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, AfterViewInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
 import { Http, Headers, RequestOptions } from '@angular/http';
@@ -6,16 +6,20 @@ import { OAuthSettings } from '../../oauthsettings';
 
 import { Router } from '@angular/router';
 
+import { ReCaptchaService, ReCaptchaParamsInterface } from '../../reCaptchaCallback'
+
 @Component({
   selector: 'app-register',
   templateUrl: 'app/login/register/register.component.html'
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements AfterViewInit {
   valForm: FormGroup;
   passwordForm: FormGroup;
 
-  constructor(public fb: FormBuilder, private http: Http, private router: Router) {
+  constructor(public fb: FormBuilder
+    , private http: Http
+    , private router: Router
+    , private reCaptchaService: ReCaptchaService) {
 
     let password = new FormControl('', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9]{6,10}$')]));
     let certainPassword = new FormControl('', CustomValidators.equalTo(password));
@@ -31,8 +35,34 @@ export class RegisterComponent {
       'lname': [null, Validators.required],
       'fname': [null, Validators.required],
       'accountagreed': [null, Validators.required],
-      'passwordGroup': this.passwordForm
+      'passwordGroup': this.passwordForm,
+      'ggl-recaptcha-input': [null, Validators.required]
     });
+  }
+
+  ngAfterViewInit(): void {
+    var self = this;
+
+    var reCaptchaElement: HTMLElement = <HTMLElement>document.querySelector("#ggl-recaptcha");
+    if (reCaptchaElement) {
+      let params: ReCaptchaParamsInterface = {
+        sitekey: "6Lfy0xMUAAAAAFl75Kn67YGjr29FB7GsZ_M1espF",
+        callback: (recaptchaToken) => {
+          self.valForm.controls['ggl-recaptcha-input'].setValue(recaptchaToken);
+        },
+        'expired-callback': () => {
+          console.warn("recaptcha is expired");
+          this.reCaptchaService.reset();
+          self.valForm.controls['ggl-recaptcha-input'].setValue(null);
+        },
+        'error-callback': () => {
+          console.error("recaptcha error");
+          this.reCaptchaService.reset();
+          self.valForm.controls['ggl-recaptcha-input'].setValue(null);
+        }
+      }
+      this.reCaptchaService.render(reCaptchaElement, params);
+    }
   }
 
   submitForm($ev, value: any) {
@@ -44,9 +74,7 @@ export class RegisterComponent {
       this.passwordForm.controls[c].markAsTouched();
     }
 
-    if (!this.valForm.valid
-      || value.passwordGroup.password !== value.passwordGroup.confirmPassword) return;
-
+    if (!this.valForm.valid || value.passwordGroup.password !== value.passwordGroup.confirmPassword) return;
 
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
@@ -54,7 +82,8 @@ export class RegisterComponent {
 
     let body = {
       Email: value.email,
-      Password: value.passwordGroup.password
+      Password: value.passwordGroup.password,
+      RecaptchaToken: value["ggl-recaptcha-input"]
     };
 
     this.http.post(OAuthSettings.api_user_management_registration_url, body, options)
@@ -64,6 +93,5 @@ export class RegisterComponent {
         this.router.navigate(['/login']);
       }
       );
-
   }
 }
---
+++ src/app/reCaptchaCallback.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+@Injectable()
+
+export class ReCaptchaService {
+
+  reCaptchaCallback_Promise: Promise<any>;
+
+  constructor() {
+    this.reCaptchaCallback_Promise = (<any>window).reCaptchaCallback_Promise;
+  }
+
+  reset(): void {
+    this.reCaptchaCallback_Promise.then((grecaptchaObj) => grecaptchaObj.reset());
+  }
+
+  getOnLoadPromise(): Promise<any> {
+    return this.reCaptchaCallback_Promise;
+  }
+
+  render(el: HTMLElement, params: ReCaptchaParamsInterface): void {
+    this.reCaptchaCallback_Promise.then((grecaptchaObj) => { grecaptchaObj.render(el, params) });
+  }
+}
+
+export interface ReCaptchaParamsInterface {
+  sitekey: string;
+  callback(recaptchaToken: string): void;
+  ['expired-callback'](): void;
+  ['error-callback'](): void;
+}
